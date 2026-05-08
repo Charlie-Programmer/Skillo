8d3ed2fc2016eafe79870ca8214ab566016ad732
charlie <charliepagapong@gmail.com>
2026-05-08 11:45:34 +0800

    May 8 2nd Update

--- lib/edit_course.dart
+++ lib/edit_course.dart
@@ -838,20 +838,24 @@ class _EditCoursePageState extends State<EditCoursePage> {
                             height: 55,
                             child:
                                 ElevatedButton(
-                              onPressed: () async {
+                              onPressed:
+                                  () async {
+
                                 cleanEmptyData();
 
-                                widget.course["isPublished"] = true;
-
-                                // 👇 ADD THIS
-                                widget.course["students"] ??= 0;
-                                widget.course["rating"] ??= 4.7;
+                                widget.course[
+                                        "isPublished"] =
+                                    true;
 
                                 await saveCleanedData();
 
-                                ScaffoldMessenger.of(context).showSnackBar(
+                                ScaffoldMessenger.of(
+                                        context)
+                                    .showSnackBar(
                                   const SnackBar(
-                                    content: Text("Course Published"),
+                                    content: Text(
+                                      "Course Published",
+                                    ),
                                   ),
                                 );
                               },
